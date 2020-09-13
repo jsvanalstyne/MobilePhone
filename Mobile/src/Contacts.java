670b6b440314a697277063ff4d8bb96e13196727
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-09-13 11:15:43 -0400

    created constructors in mobile and contact class

--- Mobile/src/Contacts.java
+++ Mobile/src/Contacts.java
@@ -1,4 +1,21 @@
-public class Contacts extends Mobile {
+public class Contacts{
+    private String name;
+    private String phoneNumber;
+//Constuctor for the contacts class
+    public Contacts(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+//    Calls on the constructor to create a new contact record.
+    public static Contacts createContactRecord(String name, String phoneNumber){
+        return new Contacts(name, phoneNumber);
+    }
 }
--- Mobile/src/Mobile.java
+++ Mobile/src/Mobile.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+
 public class Mobile {
+
+    private ArrayList <Contacts> contacts;
+    private String myNumber;
+
+    public Mobile(String myNumber) {
+        this.myNumber = myNumber;
+//        initialize the contact ArrayList;
+        this.contacts = new ArrayList<Contacts>();
+    }
+
+    public boolean addNewContact(Contacts contact){
+        if(searchContacts(contact.getName()) >=0){
+            System.out.println("Contact already in list.");
+            return false;
+        }
+        contacts.add(contact);
+        return true;
+    }
+    public int findContact(Contacts contact){
+        return
+    }
+    public int  searchContacts(String contact){
+        int contactIndex = contacts.indexOf(contact);
+        if (contactIndex >= 0){
+            return contactIndex;
+        }else{
+            return -1;
+        }
+
+
+    }
 
 }
