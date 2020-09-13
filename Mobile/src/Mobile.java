41047871a03a435548378c5cd87daac9a9d0acfa
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-09-13 11:28:16 -0400

    added search and updated functionality in mobile class

--- Mobile/src/Mobile.java
+++ Mobile/src/Mobile.java
@@ -12,25 +12,37 @@ public class Mobile {
     }
 
     public boolean addNewContact(Contacts contact){
-        if(searchContacts(contact.getName()) >=0){
+        if(findContact(contact.getName()) >=0){
             System.out.println("Contact already in list.");
             return false;
         }
         contacts.add(contact);
         return true;
     }
+//    method to find a contact. Receives the contact RECORD utilizing the contact class. Returns integer of the index of the contact if present.
     public int findContact(Contacts contact){
-        return
+        return this.contacts.indexOf(contacts);
     }
-    public int  searchContacts(String contact){
-        int contactIndex = contacts.indexOf(contact);
-        if (contactIndex >= 0){
-            return contactIndex;
-        }else{
-            return -1;
+//    Looking for a contact based on name.
+    public int  findContact(String contactName){
+        for(int i=0; i<this.contacts.size(); i++){
+            Contacts contact = this.contacts.get(i);
+            if(contact.getName() == contactName){
+                return i;
+            }
         }
+        return -1;
 
-
+    }
+    public boolean updateContact(Contacts oldContact, Contacts newContact){
+        int foundOldPosition = findContact(oldContact);
+        if(foundOldPosition < 0){
+            System.out.println("Searched contact cannot be found");
+            return false;
+        }
+        this.contacts.set(foundOldPosition, newContact);
+        System.out.println("Contact " + oldContact+ " has been updated to "+ newContact);
+        return true;
     }
 
 }
